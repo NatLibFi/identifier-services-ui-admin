ac3ecf425a3fd54ae4dc376df93691062d1ddfe6
aatuny <27770287+aatuny@users.noreply.github.com>
2024-03-12 10:55:06 +0200

    Use TableResultWrapper in IsbnMessageList

--- src/frontend/views/isbn-registry/messages/IsbnMessageList.jsx
+++ src/frontend/views/isbn-registry/messages/IsbnMessageList.jsx
@@ -38,9 +38,9 @@ import moment from 'moment';
 
 import '/src/frontend/css/common.css';
 
-import Spinner from '/src/frontend/components/common/Spinner.jsx';
-import TableComponent from '/src/frontend/components/common/TableComponent.jsx';
 import SearchComponent from '/src/frontend/components/common/SearchComponent.jsx';
+import TableComponent from '/src/frontend/components/common/TableComponent.jsx';
+import TableResultWrapper from '/src/frontend/components/common/TableResultWrapper.jsx';
 
 function IsbnMessageList() {
   const history = useHistory();
@@ -76,6 +76,9 @@ function IsbnMessageList() {
     authenticationToken
   });
 
+  const formattedData = useMemo(() => data.results.map(formatSearchResult), [data]);
+  const hasData = formattedData && formattedData.length > 0;
+
   function updateRowsPerPage(rowsPerPage) {
     updateSearchBody({limit: rowsPerPage, offset: 0});
   }
@@ -105,7 +108,6 @@ function IsbnMessageList() {
   ], []);
 
   // Filters data to be shown in the table
-  const formattedData = useMemo(() => data.results.map(formatSearchResult), [data]);
   function formatSearchResult(item) {
     const {id, recipient, subject, messageTemplateName, sent} = item;
     return {
@@ -117,27 +119,6 @@ function IsbnMessageList() {
     };
   }
 
-
-  if (error) {
-    return (
-      <Typography variant="h2" className="normalTitle">
-        <FormattedMessage id="errorPage.message.defaultError" />
-      </Typography>
-    );
-  }
-
-  if (loading) {
-    return <Spinner />;
-  }
-
-  if (data.totalDoc === 0) {
-    return (
-      <p>
-        <FormattedMessage id="common.noData" />
-      </p>
-    );
-  }
-
   return (
     <div className="listSearch">
       <Typography variant="h5">
@@ -147,17 +128,19 @@ function IsbnMessageList() {
         initialValue={searchBody.searchText}
         searchFunction={updateSearchText}
       />
-      <TableComponent
-        pagination
-        data={formattedData}
-        handleTableRowClick={handleTableRowClick}
-        headRows={headRows}
-        page={searchBody.offset !== 0 ? searchBody.offset / searchBody.limit : 0}
-        setPage={updatePageNumber}
-        totalDoc={data.totalDoc}
-        rowsPerPage={searchBody.limit}
-        setRowsPerPage={updateRowsPerPage}
-      />
+      <TableResultWrapper error={error} loading={loading} hasData={hasData}>
+        <TableComponent
+          pagination
+          data={formattedData}
+          handleTableRowClick={handleTableRowClick}
+          headRows={headRows}
+          page={searchBody.offset !== 0 ? searchBody.offset / searchBody.limit : 0}
+          setPage={updatePageNumber}
+          totalDoc={data.totalDoc}
+          rowsPerPage={searchBody.limit}
+          setRowsPerPage={updateRowsPerPage}
+        />
+      </TableResultWrapper>
     </div>
   );
 }
